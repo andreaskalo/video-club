5da5d122c4b075fd4152cc143bd932ebd49d792e
Andreas Kalo <andreaskalo001@gmail.com>
2026-07-22 14:36:39 +0300

    create movies and series categories view, create  path for movies and series details

---
+++ public/js/card.js
@@ -0,0 +1,14 @@
+// document.addEventListener("click", (event) => {
+//   const clickedCard = event.target.closest(".card_container");
+
+//   if (!clickedCard) {
+//     return;
+//   }
+
+//   const itemId = clickedCard.dataset.itemId;
+//   const isMovie = clickedCard.dataset.isMovie;
+
+//   if(isMovie){
+//     window.
+//   }
+// });
